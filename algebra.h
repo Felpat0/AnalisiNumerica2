0bf10156b310a609ebc9f43a6795aca0435308f4
Felpat0 <b994@live.com>
2020-05-31 23:31:11 +0200

    First version
    Works only for the first step

---
+++ algebra.h
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <vector>
+#include <math.h> 
+#include <cstdio>
+
+using namespace std;
+
+class myVector{
+    public:
+    unsigned int dimension;
+    vector<long double> myvector;
+
+    myVector(unsigned int d) : dimension(d){
+        myvector.resize(dimension);
+        for (unsigned i = 0; i < dimension; i++){
+            myvector[i] = 0;
+        }
+    }
+
+    void setVector(){
+        for(int j = 0; j != dimension; j++){
+                cout<<"Element "<<j<<" :";
+                cin>>this->myvector[j];
+        }
+    }
+
+    void printVector() const{
+        for(int j = 0; j != dimension; j++){
+            cout<<myvector[j]<<endl;
+        }
+    }
+
+    long double norm2() const{
+        long double temp = 0;
+        for(int i = 0; i != dimension; i ++)
+            temp += myvector[i] * myvector[i];
+        return sqrt(temp);
+    }
+
+    myVector operator+(myVector other){
+        for(int i = 0; i != dimension; i ++)
+            this->myvector[i] += other.myvector[i];
+        return (*this);
+    }
+
+    myVector operator-(myVector other){
+        for(int i = 0; i != dimension; i ++)
+            this->myvector[i] -= other.myvector[i];
+        return (*this);
+    }
+};
+
+class Matrix{
+    public:
+    unsigned int dimension;
+    vector<vector<long double>> matrix;
+
+    Matrix(unsigned int n) : dimension(n) {
+		matrix.resize(n);
+
+        for (unsigned i = 0; i < matrix.size(); i++){
+            matrix[i].resize(n, 0);
+        }
+    }
+
+    Matrix(unsigned int n, char a) : dimension(n) {
+		matrix.resize(n);
+
+        for (unsigned int i = 0; i < matrix.size(); i++){
+            matrix[i].resize(n, 0);
+        }
+
+        for (unsigned int i = 0; i < matrix.size(); i++){
+            matrix[i][i] = 1;
+        }
+    }
+
+    Matrix (myVector first, myVector other) : dimension(first.dimension){
+        matrix.resize(first.dimension);
+        for (int i = 0; i < matrix.size(); i++){
+            matrix[i].resize(first.dimension, 0);
+        }
+        for(int i = 0; i != first.dimension; i ++){
+            for(int j = 0; j != first.dimension; j++){
+                matrix[i][j] = first.myvector[i] * other.myvector[j];
+            }
+        }
+    }
+
+    void setMatrix(){
+        for(int i = 0; i != dimension; i++){
+            for(int j = 0; j != dimension; j++){
+                cout<<"Element "<<i<<","<<j<<" :";
+                cin>>this->matrix[i][j];
+            }
+        }
+    }
+
+    void printMatrix() const{
+        for(int i = 0; i != dimension; i++){
+            for(int j = 0; j != dimension; j++){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    myVector getColumn(int j){
+        myVector temp = myVector(dimension);
+        for(int i = 0; i != dimension; i++){
+            temp.myvector[i] = this->matrix[i][j];
+        }
+        return temp;
+    }
+
+    Matrix operator*(Matrix other){
+        Matrix temp = Matrix(other.dimension);
+        for(int i = 0; i != other.dimension; i++){
+            for(int j = 0; j != other.dimension; j++){
+                for(int k = 0; k != other.dimension; k++){
+                    temp.matrix[i][j] += (this->matrix)[i][k] * other.matrix[k][j];
+                }
+            }
+        }
+
+        return temp;
+    }
+
+    Matrix operator+(Matrix other){
+        for(int i = 0; i != dimension; i++){
+            for(int j = 0; j != dimension; j++){
+                matrix[i][j] += other.matrix[i][j];
+            }
+        }
+        return (*this);
+    }
+
+    Matrix operator-(Matrix other){
+        for(int i = 0; i != dimension; i++){
+            for(int j = 0; j != dimension; j++){
+                matrix[i][j] -= other.matrix[i][j];
+            }
+        }
+        return (*this);
+    }
+
+    Matrix operator*(long double other){
+        for(int i = 0; i != dimension; i++){
+            for(int j = 0; j != dimension; j++){
+                matrix[i][j] *= other;
+            }
+        }
+        return (*this);
+    }
+
+    Matrix operator/(long double other){
+        for(int i = 0; i != dimension; i++){
+            for(int j = 0; j != dimension; j++){
+                matrix[i][j] /= other;
+            }
+        }
+        return (*this);
+    }
+
+    
+    
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
+#include "algebra.h"
 
 int main(){
-    std::cout<<"lol";
+    Matrix m = Matrix(4);
+    //Set the values of the matrix
+    m.setMatrix();
+    int i = 0;
+    for(int i = 0; i != m.dimension -1; i++){
+        myVector app = myVector(m.dimension);
+        myVector temp = myVector(m.dimension - i);
+        //Calculate the u tilde vector of this column
+        app = m.getColumn(i);
+        myVector u = myVector(m.dimension - i);
+        for(int k = 0; k != m.dimension-i; k++){
+            u.myvector[k] = temp.myvector[k + i];
+        }
+        temp.myvector[0] = u.norm2();
+        u = u - temp;
+        //Calculate alpha
+        long double alpha = 2/(u.norm2()*u.norm2());
+        //Calculate the Householder matrix and apply it to m
+        Matrix householder = (Matrix(m.dimension, 'i') - (Matrix(u, u) * alpha));
+        m = householder * m;
+
+        //Put the right 0s (because of the not perfect precision)
+        for(int j = i+1; j != m.dimension; j++){
+            m.matrix[j][i] = 0;
+        }
+    }
+    /*
+    //Put the right 0s again (because of the not perfect precision)
+    for(int i = 0; i != m.dimension -1; i++){
+        for(int j = i+1; j != m.dimension; j++){
+            m.matrix[j][i] = 0;
+        }
+    }*/
+
+    m.printMatrix();
     return 0;
 }
